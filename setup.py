65e13b10fe140a6ddad30f2168a8836c463de95f
Joe Fair <joe@fairanswers.com>
2019-02-21 03:05:28 -0500

    Fixing url

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fair_flow",
-    version="0.0.5",
+    version="0.0.6",
     author="Joe Fair",
     author_email="joe@fairanswers.com",
     description="Simple Workflow Library",
-    url="https://fairanswers.com",
+    url="http://fairanswers.com",
     dependency_links=['git+https://github.com/timtadh/dot_tools@master#egg=dot_tools-0.1'],
     packages=setuptools.find_packages(),
     classifiers=[
